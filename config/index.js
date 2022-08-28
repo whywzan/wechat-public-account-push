17f98ef3919fc49067a39df6e9345ef167bb0724
whywzan <81540334+whywzan@users.noreply.github.com>
2022-08-28 09:20:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ export const config = {
     city: "湛江",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "2001", "date": "08-28", "type": 'new'},
+      {"name": "老婆", "year": "2001", "date": "09-10", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2020-05-09",
