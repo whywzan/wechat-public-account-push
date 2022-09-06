0c0903a8b0f708318d85a09399000b4121939d8c
whywzan <81540334+whywzan@users.noreply.github.com>
2022-09-07 00:14:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
     // 公众号appSecret
     appSecret: "50e08a0dc4278a5a59e3c0512f468432",
     // 模板消息id
-    templateId: "P1vi1PLeKNV45I4DhaSqhaMvxMR_uQGjLmKosYVHIdE",
+    templateId: "JWiI1thCW5BxotueYzkUxnTcsDJUEhGhCY7qUx4zDQE",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["o2AlR5y0uUN6OYVmHM6-ZnH6Ew4c"],
      
@@ -27,8 +27,6 @@ export const config = {
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
